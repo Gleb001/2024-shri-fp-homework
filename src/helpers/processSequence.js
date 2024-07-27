d57c82496f879c836012be1f2ae2172277009bbe
gleb001 <lichagingleb@mail.ru>
2024-07-27 20:48:00 +0500

    complete homework

--- src/helpers/processSequence.js
+++ src/helpers/processSequence.js
@@ -1,3 +1,4 @@
+// Задание ================================================== //
 /**
  * @file Домашка по FP ч. 2
  *
@@ -14,38 +15,248 @@
  * Иногда промисы от API будут приходить в состояние rejected, (прямо как и API в реальной жизни)
  * Ответ будет приходить в поле {result}
  */
- import Api from '../tools/api';
 
- const api = new Api();
+// Примечание =============================================== //
+/**
+ * Привет!
+ * В задании приведены два решения, где:
+ * 1) без длинной цепочки вызовов;
+ * 2) с длинной цепочкой вызовов.
+ */
 
- /**
-  * Я – пример, удали меня
-  */
- const wait = time => new Promise(resolve => {
-     setTimeout(resolve, time);
- })
+// Библиотека =============================================== //
+import Api from '../tools/api';
 
- const processSequence = ({value, writeLog, handleSuccess, handleError}) => {
-     /**
-      * Я – пример, удали меня
-      */
-     writeLog(value);
+// Вспомогательные функции ================================== //
 
-     api.get('https://api.tech/numbers/base', {from: 2, to: 10, number: '01011010101'}).then(({result}) => {
-         writeLog(result);
-     });
+/**
+ * Проверяет на истинность все переданные предикаты.
+ * 
+ * @param {Array<function(): boolean>} conditions - список предикатов.
+ * @returns {boolean} - результат проверки: true, если все предикаты истинны, иначе false.
+ */
+const allPass = conditions => conditions.every(condition => condition());
 
-     wait(2500).then(() => {
-         writeLog('SecondLog')
+/**
+ * Создает композицию из функций, вызываемых справа налево.
+ * 
+ * @param {...function} functions - Функции для композиции.
+ * @returns {function} - Функция, принимающая начальное значение и возвращающая результат скомпонованных функций.
+ */
+const compose = (...functions) => (initialValue) => (
+    functions.reduceRight((accumulator, func) => {
+        let newValue = func(accumulator);
+        if (newValue !== undefined) {
+            return newValue;
+        } else {
+            return accumulator;
+        }
+    }, initialValue)
+);
 
-         return wait(1500);
-     }).then(() => {
-         writeLog('ThirdLog');
+/**
+ * Композиция нескольких асинхронных функций справа налево.
+ *
+ * @param {...Function} asyncFunctions - Асинхронные функции для композиции.
+ * @returns {Function} - Функция, которая принимает начальное значение и возвращает промис, который разрешается в результат скомпонованных функций.
+ */
+const asyncCompose = (...asyncFunctions) => async (initialValue) => (
+    await asyncFunctions.reduceRight(async (accumulator, func) => {
+        let newValue = await func(await accumulator);
+        if (newValue !== undefined) {
+            return newValue;
+        } else {
+            return accumulator;
+        }
+    }, Promise.resolve(initialValue))
+);
 
-         return wait(400);
-     }).then(() => {
-         handleSuccess('Done');
-     });
- }
+/**
+ * Композиция нескольких асинхронных функций слева направо.
+ * 
+ * @param {...Function} asyncFunctions - Асинхронные функции для композиции.
+ * @returns {Function} - Функция, которая принимает начальное значение и возвращает промис, который разрешается в результат скомпонованных функций.
+ */
+const asyncComposit = (...asyncFunctions) => async (initialValue) => {
+    let prevValue = initialValue;
+    return await asyncFunctions.reduce(async (accumulator, func) => {
+        if (await accumulator !== undefined) prevValue = accumulator;
+        return func(await accumulator || await prevValue);
+    }, initialValue)
+};
+
+/**
+ * Функция валидации условий.
+ * 
+ * @param {Function} handleError - Функция для обработки ошибок.
+ * @returns {Function} - Функция, принимающая условие и сообщение, возвращающая функцию для проверки условия.
+ */
+const validateCondition = (handleError) => (condition, message) => () => {
+    if (condition()) {
+        handleError(message);
+        return false;
+    } else {
+        return true;
+    }
+}
+
+/**
+ * Получает результат API запроса.
+ * 
+ * @param {Function} responseApi - Функция API запроса.
+ * @returns {Promise<*>} - Промис, разрешающийся в результат API запроса.
+ */
+const getResultApi = async (responseApi) => await responseApi.result;
+
+/**
+ * Многократно вызывает API до получения успешного результата или достижения максимального количества попыток.
+ *
+ * @param {Function} api - Функция API, которая должна возвращать промис, разрешающийся в объект с полем 'result'.
+ * @param {number} [steps=5] - Максимальное количество попыток вызова API.
+ * @returns {Function} - Функция, принимающая данные и возвращающая ответ API в случае успеха, или объект с null результатом при неудаче.
+ */
+const getSuccessResultApi = (api, steps = 5) => async (data) => {
+    for (let attempt = 0; attempt < steps; attempt++) {
+        try {
+            const response = await api(data);
+            if (response.result) {
+                return response;
+            }
+        } catch (error) {
+            throw new Error(error.message);
+        }
+    }
+};
+
+// функции для расчета значений
+const getSquared = (value) => Math.pow(value, 2);
+const getRemainderDivisionOn = (divisor) => (value) => value % divisor;
+const getLength = (string) => string.length;
+const charsIn = (string) => (condition) => condition(string);
+
+// функции-предикаты (булевые функции)
+const isLessThan = (max) => (string) => string.length < max;
+const isMoreThan = (min) => (string) => min < string.length;
+const isNegative = (number) => number < 0;
+const hasDigitAndPoint = (string) => /^\d+(\.\d+)?$/.test(string);
+const not = (boolean) => !boolean;
+
+// функции-ручки (получение данных через api)
+const api = new Api();
+const getBinary = async (value) => await api.get(
+    'https://api.tech/numbers/base',
+    {
+        from: 10,
+        to: 2,
+        number: value
+    }
+);
+const getAnimal = async (idAnimal) => await api.get(
+    `https://animals.tech/${idAnimal}`,
+    {}
+);
+
+// Решение 1 ================================================ //
+
+// const processSequence = async ({ value, writeLog, handleSuccess, handleError }) => {
+
+//     // 1. Берем строку N. Пишем изначальную строку в writeLog
+//     writeLog(value);
+
+//     // 2. Строка валидируется по правилам
+//     const checkCondition = validateCondition(handleError);
+//     const charsInValue = charsIn(value);
+//     const isValidValue = allPass(
+//         [
+//             checkCondition(() => compose(isNaN, Number)(value), "строка не является числом"),
+//             checkCondition(() => charsInValue(isMoreThan(9)), "кол-во символов в числе должно быть меньше 10"),
+//             checkCondition(() => charsInValue(isLessThan(3)), "кол-во символов в числе должно быть больше 2"),
+//             checkCondition(() => compose(isNegative, Number)(value), "число должно быть положительным"),
+//             checkCondition(() => compose(not, hasDigitAndPoint)(value), "символы в строке только [0-9] и точка"),
+//         ]
+//     );
+//     if (!isValidValue) return;
+
+//     try {
+
+//         // 3. Привести строку к числу, округлить к ближайшему целому с точностью до единицы, записать в writeLog
+//         const roundedValue = compose(writeLog, Math.round, Number)(value);
+
+//         // 4. C помощью API /numbers/base перевести из 10-й системы счисления в двоичную, результат записать в writeLog
+//         const binaryValue = await asyncCompose(writeLog, getResultApi, getBinary)(roundedValue);
+
+//         // 5. Взять кол-во символов в полученном от API числе записать в writeLog
+//         const lengthBits = compose(writeLog, getLength)(binaryValue);
+
+//         // 6. Возвести в квадрат с getRemainderDivisionпомощью Javascript записать в writeLog
+//         const squareNumber = compose(writeLog, getSquared)(lengthBits);
+
+//         // 7. Взять остаток от деления на 3, записать в writeLog
+//         const remainderDivision = compose(writeLog, getRemainderDivisionOn(3))(squareNumber);
+
+//         // 8. C помощью API /animals.tech/id/name получить случайное животное используя полученный остаток в качестве id
+//         const animal = await asyncCompose(getResultApi, getAnimal)(remainderDivision);
+
+//         // 9. Завершить цепочку вызовом handleSuccess в который в качестве аргумента положить результат полученный на предыдущем шаге
+//         handleSuccess(animal);
+
+//     } catch (error) {
+//         handleError(error.message);
+//     }
+
+// }
+
+// Решение 2 ================================================ //
+
+const processSequence = async ({ value, writeLog, handleSuccess, handleError }) => {
+
+    // 1. Берем строку N. Пишем изначальную строку в writeLog
+    writeLog(value);
+
+    // 2. Строка валидируется по правилам
+    const checkCondition = validateCondition(handleError);
+    const charsInValue = charsIn(value);
+    const isValidValue = allPass(
+        [
+            checkCondition(() => compose(isNaN, Number)(value), "строка не является числом"),
+            checkCondition(() => charsInValue(isMoreThan(9)), "кол-во символов в числе должно быть меньше 10"),
+            checkCondition(() => charsInValue(isLessThan(3)), "кол-во символов в числе должно быть больше 2"),
+            checkCondition(() => compose(isNegative, Number)(value), "число должно быть положительным"),
+            checkCondition(() => compose(not, hasDigitAndPoint)(value), "символы в строке только [0-9] и точка"),
+        ]
+    );
+    if (!isValidValue) return;
+
+    try {
+        await asyncComposit(
+            // 3. Привести строку к числу, округлить к ближайшему целому с точностью до единицы, записать в writeLog
+            Number,
+            Math.round,
+            writeLog,
+            // 4. C помощью API /numbers/base перевести из 10-й системы счисления в двоичную, результат записать в writeLog
+            getSuccessResultApi(getBinary, 10),
+            getResultApi,
+            writeLog,
+            // 5. Взять кол-во символов в полученном от API числе записать в writeLog
+            getLength,
+            writeLog,
+            // 6. Возвести в квадрат с getRemainderDivisionпомощью Javascript записать в writeLog
+            getSquared,
+            writeLog,
+            // 7. Взять остаток от деления на 3, записать в writeLog
+            getRemainderDivisionOn(3),
+            writeLog,
+            // 8. C помощью API /animals.tech/id/name получить случайное животное используя полученный остаток в качестве id
+            getSuccessResultApi(getAnimal, 10),
+            getResultApi,
+            // 9. Завершить цепочку вызовом handleSuccess в который в качестве аргумента положить результат полученный на предыдущем шаге
+            handleSuccess
+        )(value);
+        // getSuccessResultApi
+    } catch (error) {
+        handleError(error.message);
+    }
+
+}
 
 export default processSequence;
--- src/helpers/validators.js
+++ src/helpers/validators.js
@@ -1,3 +1,4 @@
+// Задание ================================================== //
 /**
  * @file Домашка по FP ч. 1
  *
@@ -13,38 +14,299 @@
  * Если какие либо функции написаны руками (без использования библиотек) это не является ошибкой
  */
 
-// 1. Красная звезда, зеленый квадрат, все остальные белые.
-export const validateFieldN1 = ({star, square, triangle, circle}) => {
-    if (triangle !== 'white' || circle !== 'white') {
-        return false;
-    }
+// Примечание =============================================== //
+/**
+ * Привет!
+ * В задании приведены два решения, где:
+ * 1) То как необходимо было сделать по заданию;
+ * 2) То как бы я написал, если бы не придерживался 
+ * условию задания ( сделал, так как было больно смотреть на 
+ * 1 решение ;) ).
+ */
 
-    return star === 'red' && square === 'green';
-};
+// Решение 1 ================================================ //
+
+// Вспомогательные функции ---------------------------------- //
+
+/**
+ * Проверяем на истинность все переданные предикаты:
+ * true - все предикаты истинны
+ * false - один из предикатов ложен
+ * 
+ * @param {Array<function(): boolean>} predicates - список предикатов
+ * @returns {boolean} - результат проверки
+ */
+const allPass = conditions => conditions.every(condition => condition());
+
+/**
+ * Проверяем, что хотя бы один из предикатов истин:
+ * true - один из предикатов истин
+ * false - все предикаты ложны 
+ * 
+ * @param {Array<function(): boolean>} predicates - список предикатов
+ * @returns {boolean} - результат проверки
+ */
+const anyPass = conditions => conditions.some(condition => condition());
+
+/**
+ * Возвращает функцию, которая подсчитывает количество значений в объекте, 
+ * удовлетворяющих заданному условию.
+ *
+ * @param {function(any): boolean} condition - Условие, которому должны удовлетворять значения объекта.
+ * @returns {function(Object): number} Функция, принимающая объект и возвращающая количество значений, удовлетворяющих условию.
+ */
+function getLength(condition) {
+    return function (object) {
+        return (
+            Object
+                .values(object)
+                .reduce((counter, value) => {
+
+                    if (condition(value)) {
+                        counter++;
+                    }
+
+                    return counter;
+
+                }, 0)
+        );
+    }
+}
+
+/**
+ * Подсчитывает количество каждого значения в объекте и возвращает объект с этими подсчетами.
+ *
+ * @param {Object} object - Объект, значения которого будут подсчитаны.
+ * @returns {Object} Объект, где ключи - это уникальные значения исходного объекта, 
+ * а значения - количество их вхождений.
+ */
+function getCounterValues(object) {
+    return (
+        Object
+            .values(object)
+            .reduce((accumulator, value) => {
+
+                if (accumulator[value] === undefined) {
+                    accumulator[value] = 0;
+                }
+
+                accumulator[value]++;
+                return accumulator;
+
+            }, {})
+    );
+}
+
+// color functions
+const isWhite = (shape) => shape === "white";
+const isRed = (shape) => shape === "red";
+const isGreen = (shape) => shape === "green";
+const isOrange = (shape) => shape === "orange";
+const isBlue = (shape) => shape === "blue";
+const isAnyColor = (shape) => anyPass(
+    [
+        () => isWhite(shape),
+        () => isRed(shape),
+        () => isGreen(shape),
+        () => isOrange(shape),
+        () => isBlue(shape),
+    ]
+);
+
+// Пукты ---------------------------------------------------- //
+
+// 1. Красная звезда, зеленый квадрат, все остальные белые. 
+export const validateFieldN1 = ({ star, square, triangle, circle }) => (
+    allPass(
+        [
+            () => isRed(star),
+            () => isGreen(square),
+            () => isWhite(triangle),
+            () => isWhite(circle)
+        ]
+    )
+);
 
 // 2. Как минимум две фигуры зеленые.
-export const validateFieldN2 = () => false;
+export const validateFieldN2 = shapes => (
+    getLength(isGreen)(shapes) > 1
+);
 
 // 3. Количество красных фигур равно кол-ву синих.
-export const validateFieldN3 = () => false;
+export const validateFieldN3 = (shapes) => (
+    getLength(isRed)(shapes) === getLength(isBlue)(shapes)
+);
 
 // 4. Синий круг, красная звезда, оранжевый квадрат треугольник любого цвета
-export const validateFieldN4 = () => false;
+export const validateFieldN4 = ({ star, square, triangle, circle }) => (
+    allPass(
+        [
+            () => isRed(star),
+            () => isOrange(square),
+            () => isAnyColor(triangle),
+            () => isBlue(circle),
+        ]
+    )
+);
 
 // 5. Три фигуры одного любого цвета кроме белого (четыре фигуры одного цвета – это тоже true).
-export const validateFieldN5 = () => false;
+export const validateFieldN5 = (shapes) => {
+    const counters = getCounterValues(shapes);
+    const hasThreeColor = Object.values(counters).find(counter => counter >= 3);
+    return (hasThreeColor && (counters.white === undefined || counters.white < 2));
+};
 
 // 6. Ровно две зеленые фигуры (одна из зелёных – это треугольник), плюс одна красная. Четвёртая оставшаяся любого доступного цвета, но не нарушающая первые два условия
-export const validateFieldN6 = () => false;
+export const validateFieldN6 = (shapes) => (
+    allPass(
+        [
+            () => getLength(isGreen)(shapes) === 2,
+            () => getLength(isRed)(shapes) === 1,
+            () => isGreen(shapes.triangle)
+        ]
+    )
+)
 
 // 7. Все фигуры оранжевые.
-export const validateFieldN7 = () => false;
+export const validateFieldN7 = (shapes) => (
+    getLength(isOrange)(shapes) === Object.keys(shapes).length
+);
 
 // 8. Не красная и не белая звезда, остальные – любого цвета.
-export const validateFieldN8 = () => false;
+export const validateFieldN8 = ({ star, triangle, circle, square }) => (
+    allPass(
+        [
+            () => isAnyColor(triangle),
+            () => isAnyColor(square),
+            () => isAnyColor(circle),
+        ]
+    ) &&
+    !anyPass(
+        [
+            () => isRed(star),
+            () => isWhite(star),
+        ]
+    )
+);
 
 // 9. Все фигуры зеленые.
-export const validateFieldN9 = () => false;
+export const validateFieldN9 = (shapes) => (
+    getLength(isGreen)(shapes) === Object.keys(shapes).length
+);
 
 // 10. Треугольник и квадрат одного цвета (не белого), остальные – любого цвета
-export const validateFieldN10 = () => false;
+export const validateFieldN10 = ({ triangle, circle, star, square }) => (
+    allPass(
+        [
+            () => (triangle === square),
+            () => !isWhite(triangle),
+            () => isAnyColor(circle),
+            () => isAnyColor(star),
+        ]
+    )
+);
+
+// Решение 2 ================================================ //
+
+// Вспомогательные функции ---------------------------------- //
+
+// /**
+//  * Считаем количество цветов у полученных фигур
+//  * @param {{star, square, triangle, circle }} figures - фигуры, цвета которых необходимо посчитать
+//  * @param {string[]} [countColors=undefined] - цвета, которые нам необходимо посчитать
+//  * @returnscountColors
+//  */
+// function getCounterByValue(figures, countColors) {
+
+//     const [setCountColors, defaultCounterValues] = countColors.reduce(([setCountColors, defaultCounterValues], countColor) => {
+//         setCountColors.add(countColor);
+//         defaultCounterValues[countColor] = 0;
+//         return [setCountColors, defaultCounterValues];
+//     }, [new Set, {}])
+
+//     return (
+//         Object
+//             .values(figures)
+//             .reduce((result, color) => {
+//                 if (countColors === undefined || setCountColors.has(color)) {
+//                     if (!result[color]) {
+//                         result[color] = 0;
+//                     }
+//                     result[color]++;
+//                 }
+//                 return result;
+//             }, defaultCounterValues)
+//     );
+
+// }
+
+// Пукты ---------------------------------------------------- //
+
+// // 1. Красная звезда, зеленый квадрат, все остальные белые. 
+// export const validateFieldN1 = ({ star, square, triangle, circle }) => (
+//     star === "red" &&
+//     square === "green" &&
+//     triangle === "white" &&
+//     circle === "white"
+// );
+
+// // 2. Как минимум две фигуры зеленые.
+// export const validateFieldN2 = (inputFigures) => {
+//     const { green } = getCounterByValue(inputFigures, ["green"]);
+//     return (green > 1);
+// };
+
+// // 3. Количество красных фигур равно кол-ву синих.
+// export const validateFieldN3 = (inputFigures) => {
+//     const { red, blue } = getCounterByValue(inputFigures, ["red", "blue"]);
+//     return (red === blue);
+// };
+
+// // 4. Синий круг, красная звезда, оранжевый квадрат треугольник любого цвета
+// export const validateFieldN4 = ({ star, square, circle }) => (
+//     star   === "red"    &&
+//     square === "orange" &&
+//     circle === "blue"
+// );
+
+// // 5. Три фигуры одного любого цвета кроме белого (четыре фигуры одного цвета – это тоже true). !!!
+// export const validateFieldN5 = (inputFigures) => {
+//     const counterColors = getCounterByValue(inputFigures);
+//     return (
+//         Boolean(counterColors.white) < 2 &&
+//         undefined !== Object.values(counterColors).find(counter => counter >= 3)
+//     );
+// };
+
+// // 6. Ровно две зеленые фигуры (одна из зелёных – это треугольник), плюс одна красная. Четвёртая оставшаяся любого доступного цвета, но не нарушающая первые два условия
+// export const validateFieldN6 = (inputFigures) => {
+//     const { green, red } = getCounterByValue(inputFigures);
+//     return (
+//         green === 2 &&
+//         inputFigures.triangle === "green" &&
+//         red === 1
+//     );
+// };
+
+// // 7. Все фигуры оранжевые.
+// export const validateFieldN7 = (inputFigures) => {
+//     const { orange } = getCounterByValue(inputFigures, ["orange"]);
+//     return (orange === Object.keys(inputFigures).length);
+// };
+
+// // 8. Не красная и не белая звезда, остальные – любого цвета.
+// export const validateFieldN8 = ({ star }) => (
+//     star !== "red" &&
+//     star !== "white"
+// );
+
+// // 9. Все фигуры зеленые.
+// export const validateFieldN9 = (inputFigures) => {
+//     const { green } = getCounterByValue(inputFigures, ["green"]);
+//     return (green === Object.keys(inputFigures).length);
+// };
+
+// // 10. Треугольник и квадрат одного цвета (не белого), остальные – любого цвета
+// export const validateFieldN10 = ({ triangle, square }) => (
+//     triangle === square
+// );
