0dd9f66524f09322d80417198974a453d0f8a3c9
gleb001 <lichagingleb@mail.ru>
2024-07-30 00:38:39 +0500

    wrongs

--- src/helpers/processSequence.js
+++ src/helpers/processSequence.js
@@ -1,3 +1,4 @@
+// Задание ================================================== //
 /**
  * @file Домашка по FP ч. 2
  *
@@ -14,38 +15,250 @@
  * Иногда промисы от API будут приходить в состояние rejected, (прямо как и API в реальной жизни)
  * Ответ будет приходить в поле {result}
  */
- import Api from '../tools/api';
 
- const api = new Api();
+// Примечание =============================================== //
+/**
+ * Привет!
+ * В задании приведены два решения, где:
+ * 1) без длинной цепочки вызовов;
+ * 2) с длинной цепочкой вызовов.
+ */
 
- /**
-  * Я – пример, удали меня
-  */
- const wait = time => new Promise(resolve => {
-     setTimeout(resolve, time);
- })
+// Библиотека =============================================== //
+import Api from '../tools/api';
 
- const processSequence = ({value, writeLog, handleSuccess, handleError}) => {
-     /**
-      * Я – пример, удали меня
-      */
-     writeLog(value);
+// Вспомогательные функции ================================== //
 
-     api.get('https://api.tech/numbers/base', {from: 2, to: 10, number: '01011010101'}).then(({result}) => {
-         writeLog(result);
-     });
+/**
+ * Проверяем на истинность все переданные предикаты:
+ * true - все предикаты истинны
+ * false - один из предикатов ложен
+ * 
+ * @param {Array<function(): boolean>} predicates - список предикатов
+ * @returns {boolean} - результат проверки
+ */
+const allPass = conditions => conditions.every(condition => condition());
 
-     wait(2500).then(() => {
-         writeLog('SecondLog')
+/**
+ * 
+ * @param  {...any} functions 
+ * @returns 
+ */
+const compose = (...functions) => (initialValue) => (
+    functions.reduceRight((accumulator, func) => {
+        let newValue = func(accumulator);
+        if (newValue !== undefined) {
+            return newValue
+        } else {
+            return accumulator;
+        }
+    }, initialValue)
+);
 
-         return wait(1500);
-     }).then(() => {
-         writeLog('ThirdLog');
 
-         return wait(400);
-     }).then(() => {
-         handleSuccess('Done');
-     });
- }
+/**
+ * Композиция нескольких асинхронных функций справа налево.
+ *
+ * @param {...Function} asyncFunctions - Асинхронные функции для композиции.
+ * @returns {Function} Функция, которая принимает начальное значение и возвращает промис, который разрешается в результат скомпонованных функций.
+ */
+const asyncCompose = (...asyncFunctions) => async (initialValue) => (
+    await asyncFunctions.reduceRight(async (accumulator, func) => {
+        let newValue = await func(await accumulator);
+        if (newValue !== undefined) {
+            return newValue;
+        } else {
+            return accumulator;
+        }
+    }, Promise.resolve(initialValue))
+);
+
+/**
+ * 
+ * @param  {...any} functions 
+ * @returns 
+ */
+const asyncComposit = (...asyncFunctions) => async (initialValue) => {
+    let prevValue = initialValue;
+    return await asyncFunctions.reduce(async (accumulator, func) => {
+        if (await accumulator !== undefined) prevValue = accumulator;
+        return func(await accumulator || await prevValue);
+    }, initialValue)
+};
+
+/**
+ * 
+ * @param  {...any} functions 
+ * @returns 
+ */
+const validateCondition = (handleError) => (condition, message) => () => {
+    if (condition()) {
+        handleError(message);
+        return false;
+    } else {
+        return true;
+    }
+}
+
+/**
+ * 
+ * @param  {...any} functions 
+ * @returns 
+ */
+const getResultApi = async (responseApi) => await responseApi.result;
+
+
+/**
+ * Repeatedly calls an API until a successful result is achieved or the maximum number of steps is reached.
+ *
+ * @param {function} api - The API function to be called. It should return a promise that resolves to an object containing a 'result' property.
+ * @param {number} steps - The maximum number of attempts to call the API.
+ * @returns {function} - A function that takes data as a parameter and returns the API response if successful, or an object with a null result if all attempts fail.
+ */
+const getSuccessResultApi = (api, steps = 5) => async (data) => {
+    for (let attempt = 0; attempt < steps; attempt++) {
+        try {
+            const response = await api(data);
+            if (response.result) {
+                return response;
+            }
+        } catch (error) {
+            // Handle or log the error if necessary
+            console.error(`Attempt ${attempt + 1} failed:`, error);
+        }
+    }
+    return { result: null };
+};
+
+// функции для расчета значений
+const getSquared = (value) => Math.pow(value, 2);
+const getRemainderDivisionOn = (divisor) => (value) => value % divisor;
+const getLength = (string) => string.length;
+const charsIn = (string) => (condition) => condition(string);
+
+// функции-предикаты (булевые функции)
+const isLessThan = (max) => (string) => string.length < max;
+const isMoreThan = (min) => (string) => min < string.length;
+const isNegative = (number) => number < 0;
+const hasDigitAndPoint = (string) => /^\d+(\.\d+)?$/.test(string);
+const not = (boolean) => !boolean;
+
+// функции-ручки (получение данных через api)
+const api = new Api();
+const getBinary = async (value) => await api.get(
+    'https://api.tech/numbers/base',
+    {
+        from: 10,
+        to: 2,
+        number: value
+    }
+);
+const getAnimal = async (idAnimal) => await api.get(
+    `https://animals.tech/${idAnimal}`,
+    {}
+);
+
+// Решение 1 ================================================ //
+
+const processSequence = async ({ value, writeLog, handleSuccess, handleError }) => {
+
+    // 1. Берем строку N. Пишем изначальную строку в writeLog
+    writeLog(value);
+
+    // 2. Строка валидируется по правилам
+    const checkCondition = validateCondition(handleError);
+    const charsInValue = charsIn(value);
+    const isValidValue = allPass(
+        [
+            checkCondition(() => compose(isNaN, Number)(value), "строка не является числом"),
+            checkCondition(() => charsInValue(isMoreThan(9)), "кол-во символов в числе должно быть меньше 10"),
+            checkCondition(() => charsInValue(isLessThan(3)), "кол-во символов в числе должно быть больше 2"),
+            checkCondition(() => compose(isNegative, Number)(value), "число должно быть положительным"),
+            checkCondition(() => compose(not, hasDigitAndPoint)(value), "символы в строке только [0-9] и точка"),
+        ]
+    );
+    if (!isValidValue) return;
+
+    try {
+
+        // 3. Привести строку к числу, округлить к ближайшему целому с точностью до единицы, записать в writeLog
+        const roundedValue = compose(writeLog, Math.round, Number)(value);
+
+        // 4. C помощью API /numbers/base перевести из 10-й системы счисления в двоичную, результат записать в writeLog
+        const binaryValue = await asyncCompose(writeLog, getResultApi, getBinary)(roundedValue);
+
+        // 5. Взять кол-во символов в полученном от API числе записать в writeLog
+        const lengthBits = compose(writeLog, getLength)(binaryValue);
+
+        // 6. Возвести в квадрат с getRemainderDivisionпомощью Javascript записать в writeLog
+        const squareNumber = compose(writeLog, getSquared)(lengthBits);
+
+        // 7. Взять остаток от деления на 3, записать в writeLog
+        const remainderDivision = compose(writeLog, getRemainderDivisionOn(3))(squareNumber);
+
+        // 8. C помощью API /animals.tech/id/name получить случайное животное используя полученный остаток в качестве id
+        const animal = await asyncCompose(getResultApi, getAnimal)(remainderDivision);
+
+        // 9. Завершить цепочку вызовом handleSuccess в который в качестве аргумента положить результат полученный на предыдущем шаге
+        handleSuccess(animal);
+
+    } catch (error) {
+        handleError(error.message);
+    }
+
+}
+
+// Решение 2 ================================================ //
+
+// const processSequence = async ({ value, writeLog, handleSuccess, handleError }) => {
+
+//     // 1. Берем строку N. Пишем изначальную строку в writeLog
+//     writeLog(value);
+
+//     // 2. Строка валидируется по правилам
+//     const checkCondition = validateCondition(handleError);
+//     const charsInValue = charsIn(value);
+//     const isValidValue = allPass(
+//         [
+//             checkCondition(() => compose(isNaN, Number)(value), "строка не является числом"),
+//             checkCondition(() => charsInValue(isMoreThan(9)), "кол-во символов в числе должно быть меньше 10"),
+//             checkCondition(() => charsInValue(isLessThan(3)), "кол-во символов в числе должно быть больше 2"),
+//             checkCondition(() => compose(isNegative, Number)(value), "число должно быть положительным"),
+//             checkCondition(() => compose(not, hasDigitAndPoint)(value), "символы в строке только [0-9] и точка"),
+//         ]
+//     );
+//     if (!isValidValue) return;
+
+//     try {
+//         await asyncComposit(
+//             // 3. Привести строку к числу, округлить к ближайшему целому с точностью до единицы, записать в writeLog
+//             Number,
+//             Math.round,
+//             writeLog,
+//             // 4. C помощью API /numbers/base перевести из 10-й системы счисления в двоичную, результат записать в writeLog
+//             getSuccessResultApi(getBinary, 10),
+//             getResultApi,
+//             writeLog,
+//             // 5. Взять кол-во символов в полученном от API числе записать в writeLog
+//             getLength,
+//             writeLog,
+//             // 6. Возвести в квадрат с getRemainderDivisionпомощью Javascript записать в writeLog
+//             getSquared,
+//             writeLog,
+//             // 7. Взять остаток от деления на 3, записать в writeLog
+//             getRemainderDivisionOn(3),
+//             writeLog,
+//             // 8. C помощью API /animals.tech/id/name получить случайное животное используя полученный остаток в качестве id
+//             getSuccessResultApi(getAnimal, 10),
+//             getResultApi,
+//             // 9. Завершить цепочку вызовом handleSuccess в который в качестве аргумента положить результат полученный на предыдущем шаге
+//             handleSuccess
+//         )(value);
+//         // getSuccessResultApi
+//     } catch (error) {
+//         handleError(error.message);
+//     }
+
+// }
 
 export default processSequence;
